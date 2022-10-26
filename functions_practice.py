ac5405c2408dee32c8542c548aba70d9a5382018
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-10-26 00:24:10 -0400

    first python project

---
+++ functions_practice.py
@@ -0,0 +1,20 @@
+def hello():
+    print("Hello, user!")
+
+def pack(item_1, item_2, item_3):
+    return[item_1, item_2, item_3]
+
+def eat_lunch(Lunch_items):
+    if len(Lunch_items)==0:
+        print("Ain't got no lunch!")
+    else:
+        for i in range(len(Lunch_items)):
+            if i == 0:
+                print(f"First I eat {Lunch_items[i]}")
+            else:
+                print(f"Next I eat {Lunch_items[i]}")
+hello()
+print(pack("a", "b", "c"))
+eat_lunch([])
+eat_lunch(["Pork Chop"])
+eat_lunch(["Pork Chop","Tandori Chicken","Beef Surprise","Sprite" ])
